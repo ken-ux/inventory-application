b1701e9d9a3ae8c902d0927eaf7b6c118958d349
Kenny Nguyen <sokenns@gmail.com>
2024-03-19 10:41:09 -0700

    Fix typing for unit price on schema

--- models/item.js
+++ models/item.js
@@ -6,7 +6,7 @@ const ItemSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   description: { type: String, required: true, maxLength: 300 },
   category: [{ type: Schema.Types.ObjectId, ref: "Category", required: true }],
-  unit_price: { type: mongoose.Types.Decimal128, required: true, min: 0.01 },
+  unit_price: { type: Schema.Types.Decimal128, required: true, min: 0.01 },
   stock: { type: Number, required: true, min: 0 },
 });
 

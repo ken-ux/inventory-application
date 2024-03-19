902063555fce62288b339ff0ddb718f60a7af37a
Kenny Nguyen <sokenns@gmail.com>
2024-03-19 13:37:45 -0700

    Increase limit for description length

--- models/category.js
+++ models/category.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
-  description: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true, maxLength: 300 },
 });
 
 CategorySchema.virtual("url").get(function () {
